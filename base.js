ad4a82dba6e51bc8bc4d2be2689bd053f3ad9f83
Konap <shaolee.web@gmail.com>
2017-04-11 01:09:33 +0900

    Update base.js

--- base.js
+++ base.js
@@ -4,6 +4,17 @@
  * @param {*} value
  * @return {Boolean}
  */
+function isDef (value) {
+    return value !== undefined && value !== null;
+}
+
+/**
+ * 値が存在しないか確認します。
+ *
+ * @param {*} value
+ * @return {Boolean}
+ */
 function isUndef (value) {
     return value === undefined || value === null;
 }
+
